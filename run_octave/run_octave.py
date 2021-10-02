8cfbcbb8bdd04b88955eb4e8544137f5fd844257
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2021-10-02 11:28:53 -0400

    Update run_octave.py

--- run_octave/run_octave.py
+++ run_octave/run_octave.py
@@ -4,20 +4,20 @@ from os import path, system
 
 class RunOctave:
 
-    __version__ = '1.0.3'
+    __version__ = "1.0.3"
 
     def __init__(self, octave_path):
-        self.octave_path = octave_path.replace(' ','" "')
-        self.alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        self.octave_path = octave_path.replace(" ","' '")
+        self.alphabet = "abcdefghijklmnopqrstuvwxyz"
 
         # Comma-separated letters
-        self.CSL = ','.join(self.alphabet)
+        self.CSL = ",".join(self.alphabet)
 
         # Getting PATH of temp files
         self.lib_path = path.split(path.abspath(__file__))[0]
         # print(self.lib_path)
-        self.tempdata_path = path.join(self.lib_path, 'tempdata.mat').replace('\\','/')
-        self.tempscript_path = path.join(self.lib_path, 'tempscript.m').replace('\\','/')
+        self.tempdata_path = path.join(self.lib_path, "tempdata.mat").replace("\\","/")
+        self.tempscript_path = path.join(self.lib_path, "tempscript.m").replace("\\","/")
 
 
     def output_formatter(self, nargout, data):
@@ -37,13 +37,13 @@ class RunOctave:
 
     def mat_file(self, nargout, syntax):
         # Auxiliary function
-        with open(self.tempscript_path, 'w') as MAT_file:
-            print('load("' + self.tempdata_path + '")', file=MAT_file)
+        with open(self.tempscript_path, "w") as MAT_file:
+            print("load('" + self.tempdata_path + "')", file=MAT_file)
             print(syntax, file=MAT_file)
-            print('save("-mat-binary","' + self.tempdata_path + '")', file=MAT_file)
+            print("save('-mat-binary','" + self.tempdata_path + "')", file=MAT_file)
 
         # Executes the auxiliary function
-        system(self.octave_path + ' ' + self.tempscript_path)
+        system(self.octave_path + " " + self.tempscript_path)
 
         # Read the communication channel
         data = loadmat(self.tempdata_path)
@@ -55,7 +55,7 @@ class RunOctave:
         if isinstance(args, tuple):
             nargin = len(args)
             varargin  = self.CSL[:nargin*2-1]
-            syntax = target + '(' + varargin + ');'
+            syntax = target + "(" + varargin + ");"
 
             # Write in the communication channel
             savemat(self.tempdata_path, dict(zip(self.alphabet[:nargin], args)))
@@ -63,13 +63,13 @@ class RunOctave:
             if any(c in target for c in "[]=(,) '+-*/:"):
                 syntax = target
             else:
-                syntax = target + '();'
+                syntax = target + "();"
 
             # Write in the communication channel
-            savemat(self.tempdata_path, {'None': []})
+            savemat(self.tempdata_path, {"None": []})
 
         if nargout > 0:
             varargout = self.CSL[:nargout*2-1]
-            syntax = '[' + varargout + '] = ' + syntax
+            syntax = "[" + varargout + "] = " + syntax
 
         return self.mat_file(nargout, syntax)
